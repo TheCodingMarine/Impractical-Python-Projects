fc9b50743e65b0d028926df7f70c3274e3359a6a
Ty <usmc.millert@gmail.com>
2022-05-02 22:48:08 -0400

    edits to conform with pep8

--- ch01/pig_latin_sentence.py
+++ ch01/pig_latin_sentence.py
@@ -1,6 +1,6 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 
-while True: 
+while True:
     sentence = input('Please enter a sentence: ').split()
     new_sentence = []
     for word in sentence:
@@ -9,12 +9,12 @@ while True:
             new_sentence.append(new_word)
         else:
             letter = word[0]
-            new_word = word[1: ] + letter + 'ay'
+            new_word = word[1:] + letter + 'ay'
             new_sentence.append(new_word)
 
     full_sentence = ' '.join(new_sentence)
     print(full_sentence)
-    
+
     keep_going = input("Would you like to enter another sentence? (y or n): ")
     if keep_going.lower() == 'n':
         break
